a6c1a10f124797a59bcefb5f857423cb31b78f2e
ZeroFuku <12mnsin@gmail.com>
2026-05-07 17:22:29 +0800

    inital file structure

--- src/main/java/com/example/csit228capstone/MainApplication.java
+++ src/main/java/com/example/csit228capstone/MainApplication.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class MainViewApplication extends Application {
+public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainViewApplication.class.getResource("MainView.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("Login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Dashboard");
+        stage.setTitle("Login");
         stage.setScene(scene);
         stage.show();
     }
