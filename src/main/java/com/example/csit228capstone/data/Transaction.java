6e8608b92fbff6937d8fda277d8348c3eb4be08a
ZeroFuku <12mnsin@gmail.com>
2026-05-12 22:52:57 +0800

    added tostring method

--- src/main/java/com/example/csit228capstone/data/Transaction.java
+++ src/main/java/com/example/csit228capstone/data/Transaction.java
@@ -1,5 +1,6 @@
 package com.example.csit228capstone.data;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Transaction {
@@ -8,14 +9,6 @@ public class Transaction {
     private String description;
     private TransactionType transactionType;
     private double amount;
-
-    public String getTransactionTitle() {
-        return transactionTitle;
-    }
-
-    public void setTransactionTitle(String transactionTitle) {
-        this.transactionTitle = transactionTitle;
-    }
 
     public  Transaction(String transactionTitle, String description, TransactionType transactionType, double amount){
         this.transactionTitle = transactionTitle;
@@ -33,37 +26,45 @@ public class Transaction {
         this.transactionType = transactionType;
     }
 
-
-
+    public String getTransactionTitle() {
+        return transactionTitle;
+    }
+    public void setTransactionTitle(String transactionTitle) {
+        this.transactionTitle = transactionTitle;
+    }
     public Date getTransactionDate() {
         return transactionDate;
     }
-
     public double getAmount() {
         return amount;
     }
-
     public void setTransactionDate(Date transactionDate) {
         this.transactionDate = transactionDate;
     }
-
     public String getDescription() {
         return description;
     }
-
     public void setDescription(String description) {
         this.description = description;
     }
-
     public TransactionType getTransactionType() {
         return transactionType;
     }
-
     public void setTransactionType(TransactionType transactionType) {
         this.transactionType = transactionType;
     }
-
     public void setAmount(double amount) {
         this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Transaction[Title: %s, Date: %s, Description: %s, Type: %s, Amount: $%.2f]",
+                transactionTitle,
+                transactionDate,
+                description,
+                transactionType,
+                amount
+        );
     }
 }
