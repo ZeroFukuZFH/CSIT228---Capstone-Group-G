b7a65bcd5c9cf3d14069edd230235a5cc67dc43c
ZeroFuku <12mnsin@gmail.com>
2026-05-14 18:50:39 +0800

    added prototype login service

---
+++ src/main/java/com/example/csit228capstone/services/LoginService.java
@@ -0,0 +1,14 @@
+package com.example.csit228capstone.services;
+
+import com.example.csit228capstone.Database.Database;
+
+public class LoginService extends Database {
+    public LoginService(){
+        super();
+    }
+    public boolean login(String firstname ,String lastname, String password){
+        return true;
+    }
+
+
+}
---
+++ src/main/java/com/example/csit228capstone/services/ServiceTester.java
@@ -0,0 +1,7 @@
+package com.example.csit228capstone.services;
+
+public class ServiceTester {
+    public static void main(String[] args) {
+        LoginService loginService = new LoginService();
+    }
+}
