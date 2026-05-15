16539cd92228b02a696d5da748341b1c6212bfa2
ZeroFuku <12mnsin@gmail.com>
2026-05-15 11:29:33 +0800

    reversed conditions for cleaner lines

--- src/main/java/com/example/csit228capstone/services/RegisterService.java
+++ src/main/java/com/example/csit228capstone/services/RegisterService.java
@@ -6,6 +6,7 @@ import com.example.csit228capstone.session.Session;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class RegisterService extends Database {
     public RegisterService(){
@@ -13,19 +14,19 @@ public class RegisterService extends Database {
     }
     public void register(String firstname,String lastname,String password){
         String sql = "INSERT INTO user (first_name,last_name,password) VALUES (?,?,?)";
-        try(PreparedStatement pstmt = super.connection.prepareStatement(sql)) {
+        try(PreparedStatement pstmt = super.connection.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS)) {
             pstmt.setString(1,firstname);
             pstmt.setString(2,lastname);
             pstmt.setString(3,password);
 
             int affectedRows = pstmt.executeUpdate();
 
-            if (affectedRows > 0) {
-                ResultSet generatedKeys = pstmt.getGeneratedKeys();
-                if (generatedKeys.next()) {
-                    int id = generatedKeys.getInt("user_id");
-                    Session.getInstance().setAttribute("id",String.valueOf(id));
-                }
+            if (!(affectedRows > 0)) return;
+
+            ResultSet generatedKeys = pstmt.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                int id = generatedKeys.getInt(1);
+                Session.getInstance().setAttribute("id",String.valueOf(id));
             }
 
         } catch (SQLException e) {
